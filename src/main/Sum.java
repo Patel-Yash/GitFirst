43c20645a20a49ed4740b7c48835568eedc1a1e8
Patel-Yash <yashpatel2236@gmail.com>
2018-01-04 22:41:01 +0530

    demo committed

---
+++ src/main/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+
+    public static void main(String[] args) {
+
+        Sum sum = new Sum();
+        sum.displayResult(20,10);
+    }
+
+    public void displayResult(int x,int y)
+    {
+        System.out.println(x+y);
+    }
+}
